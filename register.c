c949e2021c6e7c69e0d7c24f0b7420e7db3f06e9
Ali Iqbal <ali.iqbal3578@gmail.com>
2023-10-18 11:18:37 -0500

    Added prompt for coins

--- register.c
+++ register.c
@@ -2,5 +2,19 @@
 
 int main()
 {
+    int pennies;
+    int nickels;
+    int dimes;
+    int quarters;
+    
+    printf("Enter pennies: ");
+    scanf("%d", &pennies);
+    printf("Enter nickels: ");
+    scanf("%d", &nickels);
+    printf("Enter dimes: ");
+    scanf("%d", &dimes);
+    printf("Enter quarters: ");
+    scanf("%d", &quarters);    
+    
     return 0;
 }
